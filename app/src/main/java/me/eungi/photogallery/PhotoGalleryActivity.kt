d0746e3000bd1d5119a61eb9e21067dfa54608fe
eungikim <eungikim.me@gmail.com>
2021-08-19 18:20:11 +0900

    Add fragment has recyclerview

--- app/src/main/java/me/eungi/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/me/eungi/photogallery/PhotoGalleryActivity.kt
@@ -3,9 +3,19 @@ package me.eungi.photogallery
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private const val TAG = "PhotoGalleryActivity"
+
 class PhotoGalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_gallery)
+
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
     }
 }
