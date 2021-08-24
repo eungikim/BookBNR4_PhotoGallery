6443e0afc2868082c01066d81cecb7321d7a1d06
eungikim <eungikim.me@gmail.com>
2021-08-24 23:08:09 +0900

    Add BR and set permission

---
+++ app/src/main/java/me/eungi/photogallery/NotificationReceiver.kt
@@ -0,0 +1,15 @@
+package me.eungi.photogallery
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+private const val TAG = "NotificationReceiver"
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(TAG, "onReceive: received broadcast: ${intent.action}")
+    }
+}
--- app/src/main/java/me/eungi/photogallery/PollWorker.kt
+++ app/src/main/java/me/eungi/photogallery/PollWorker.kt
@@ -2,6 +2,7 @@ package me.eungi.photogallery
 
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -46,9 +47,16 @@ class PollWorker(private val context: Context, workerParams: WorkerParameters) :
 
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0, notification)
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
         }
 
         return Result.success()
+    }
+
+    companion object {
+        const val ACTION_SHOW_NOTIFICATION =
+            "me.eungi.photogallery.ACTION_SHOW_NOTIFICATION"
+        const val PERM_PRIVATE = "me.eungi.photogallery.PRIVATE"
     }
 
 }
